66e40043e765944dd9fe1bf52415088150143856
bharadwajp <bharadwajpendyala@gmail.com>
2018-11-07 21:26:00 +0530

    updated answer for package and server for bcrypt FCC

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
 'use strict';
-const express     = require('express');
-const bodyParser  = require('body-parser');
-const fccTesting  = require('./freeCodeCamp/fcctesting.js');
-const app         = express();
+const express = require('express');
+const bodyParser = require('body-parser');
+const fccTesting = require('./freeCodeCamp/fcctesting.js');
+const app = express();
+var bcrypt = require('bcrypt');
 
 fccTesting(app); //For FCC testing purposes
 
@@ -13,12 +14,19 @@ const someOtherPlaintextPassword = 'pass123';
 
 //START_ASYNC -do not remove notes, place code between correct pair of notes.
 
-
+bcrypt.hash(myPlaintextPassword, saltRounds, (err, hash) => {
+    console.log(hash); //$2a$12$Y.PHPE15wR25qrrtgGkiYe2sXo98cjuMCG1YwSI5rJW1DSJp0gEYS
+    bcrypt.compare(myPlaintextPassword, hash, (err, res) => {
+        console.log(res); //true
+    });
+});
 
 //END_ASYNC
 
 //START_SYNC
 
+var hash = bcrypt.hashSync(myPlaintextPassword, saltRounds);
+var result = bcrypt.compareSync(myPlaintextPassword, hash);
 
 
 //END_SYNC
